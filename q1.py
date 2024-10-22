8786d23a978e26b0a3579f44c208a30af22e0651
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-22 10:49:21 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+'''
+ Write a program to get a string value and print the same as output:
+Sample Input:
+Hello World
+Sample Output:
+Hello World
+'''
+
+user_input = input("Enter a string: ")
+print(user_input)
