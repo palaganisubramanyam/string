98a4eab426d749ce9a710121f5c228241392b575
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-22 10:50:56 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,12 @@
+'''
+Write a program to print th length of the given string.
+Sample Input:
+Hello
+Sample Output:
+Hello: 5
+'''
+
+
+user_input = input("Enter a string: ")
+length_of_string = len(user_input)
+print(f"{user_input}: {length_of_string}")
